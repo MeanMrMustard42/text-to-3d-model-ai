9001a97081817450b1a08519514a2feb93a927a1
MeanMrMustard42 <stevensuarez426@gmail.com>
2024-09-04 20:17:14 -0700

    got sidebar working finally. onto the AI model

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client' 
  
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import Divider from   
@@ -13,11 +13,14 @@ import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';   
 import ListItemText from '@mui/material/ListItemText';   
 import Box from '@mui/joy/Box';
+import { DialogContentText, DialogTitle } from '@mui/material';
 
 
 function Home() {
   const [query, setQuery] = useState('');
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => { document.body.style.backgroundColor = 'gray' }, []) 
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -53,34 +56,21 @@ function Home() {
       </IconButton>
 
       {/*sidebar stuff*/}
-      <Drawer anchor="right" open={isOpen} onClose={toggleDrawer} size="lg">
-      <List>
-        <ListItem button onClick={toggleDrawer}>
-          <ListItemIcon>
-            {/* Your icon here */}
-          </ListItemIcon>
-          <ListItemText primary="About" />
-        </ListItem>
-        <ListItem button onClick={toggleDrawer}>
-          <ListItemIcon>
-            {/* Your icon here */}
-          </ListItemIcon>
-          <ListItemText primary="Credits" />
-        </ListItem>
-      </List>
-      <Box
-          sx={{
-            display: 'flex',
-            gap: 1,
-            p: 1.5,
-            pb: 2,
-            borderTop: '1px solid',
-            borderColor: 'divider',
-          }}
-        ></Box>
+      <Box sx={{ display: 'flex'}}>
+      <Drawer anchor="left" open={isOpen} onClose={toggleDrawer} size="lg">
+      <DialogTitle>What is this?⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</DialogTitle>
+     <DialogContentText>⠀⠀This is an OpenAI-powered text-to-3D </DialogContentText>
+     <DialogContentText>⠀⠀model. Simply enter in a word or phrase </DialogContentText>
+     <DialogContentText>⠀⠀in the box and press return to get started!</DialogContentText>
+     <DialogTitle>Credits</DialogTitle>
+     <DialogContentText>⠀⠀Steven Suarez, B.A Applied Computing,</DialogContentText>
+     <DialogContentText>⠀⠀University of Washington, Bothell.</DialogContentText>
+     <DialogContentText>⠀⠀Model: SHAP-e Text-to-3D, OpenAI.</DialogContentText>
     </Drawer>
+    </Box>
     </Paper>
   );
+
 }
 
 export default Home;
